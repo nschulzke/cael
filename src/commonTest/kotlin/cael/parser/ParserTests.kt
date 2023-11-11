cbc7ba8ca1c79fc7d8902f00e262abddd08ba8dd
Nathan Schulzke <nschulzke@gmail.com>
2023-11-11 14:11:37 -0700

    Add tests for function declarations

--- src/commonTest/kotlin/cael/parser/ParserTests.kt
+++ src/commonTest/kotlin/cael/parser/ParserTests.kt
@@ -22,4 +22,75 @@ class ParserTests : DescribeSpec({
             )
         }
     }
+
+    describe("fun declaration") {
+        it("should parse a simple fun declaration") {
+            "fun f() => 1".lex().parse() shouldBe Program(
+                declarations = listOf(
+                    Decl.Fun.Tuple(
+                        name = "f",
+                        parameters = emptyList(),
+                        value = Expr.Literal.Int(
+                            value = 1,
+                            range = Range(12, 1),
+                        ),
+                        range = Range(1, 12),
+                    )
+                ),
+                range = Range(1, 12),
+            )
+        }
+
+        it("should parse a tuple fun declaration") {
+            "fun f(x, y) => 1".lex().parse() shouldBe Program(
+                declarations = listOf(
+                    Decl.Fun.Tuple(
+                        name = "f",
+                        parameters = listOf(
+                            Pattern.Identifier(
+                                name = "x",
+                                range = Range(7, 1),
+                            ),
+                            Pattern.Identifier(
+                                name = "y",
+                                range = Range(10, 1),
+                            ),
+                        ),
+                        value = Expr.Literal.Int(
+                            value = 1,
+                            range = Range(16, 1),
+                        ),
+                        range = Range(1, 16),
+                    )
+                ),
+                range = Range(1, 16),
+            )
+        }
+
+        it("should parse a record fun declaration") {
+            "fun f { x = Int } => 1".lex().parse() shouldBe Program(
+                declarations = listOf(
+                    Decl.Fun.Record(
+                        name = "f",
+                        parameters = listOf(
+                            PatternRecordItem(
+                                name = "x",
+                                pattern = Pattern.Identifier(
+                                    name = "Int",
+                                    range = Range(13, 3),
+                                ),
+                                range = Range(9, 7),
+                            )
+                        ),
+                        value = Expr.Literal.Int(
+                            value = 1,
+                            range = Range(22, 1),
+                        ),
+                        range = Range(1, 22),
+                    )
+                ),
+                range = Range(1, 22),
+            )
+        }
+    }
 })
