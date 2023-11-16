307c9dd73464fd176eb6e21221c83c9f95d884e4
Nathan Schulzke <nschulzke@gmail.com>
2023-11-15 20:08:41 -0700

    Set up basic analysis

---
+++ src/commonMain/kotlin/cael/analysis/Analysis.kt
@@ -0,0 +1,166 @@
+package cael.analysis
+
+import cael.ast.Range
+import cael.ast.Program as ASTProgram
+import cael.ast.Decl as ASTDecl
+import cael.ast.Expr as ASTExpr
+import cael.ast.Pattern as ASTPattern
+
+class Environment(
+    val parent: Environment? = null,
+) {
+    private val definitions: MutableMap<String, RegistryEntry> = mutableMapOf()
+
+    private data class RegistryEntry(
+        val type: Type,
+        val node: Node,
+    )
+
+    fun findType(name: String): Type? {
+        return definitions[name]?.type ?: parent?.findType(name)
+    }
+
+    fun register(name: String, type: Type, node: Node) {
+        definitions[name] = RegistryEntry(type, node)
+    }
+}
+
+fun ASTProgram.analyze(environment: Environment = Environment()): Program {
+    val newDeclarations = mutableListOf<Decl>()
+    for (decl in declarations) {
+        newDeclarations.add(decl.analyze(environment))
+    }
+    return Program(newDeclarations, range)
+}
+
+fun ASTDecl.analyze(environment: Environment): Decl {
+    return when (this) {
+        is ASTDecl.Let -> {
+            val analyzedValue = value.analyze(environment)
+            val analyzedPattern = pattern.analyze(environment, analyzedValue.type)
+            val boundNames = analyzedPattern.boundNames()
+            for (name in boundNames) {
+                environment.register(name.name, name.type, name.node)
+            }
+            Decl.Let(analyzedPattern, analyzedValue, range)
+        }
+        is ASTDecl.Fun.Match -> TODO()
+        is ASTDecl.Fun.Record -> TODO()
+        is ASTDecl.Fun.Tuple -> TODO()
+        is ASTDecl.Struct.Bare -> {
+            val type = Type.StructConstructor.Bare(name)
+            val node = Decl.Struct.Bare(type,  range)
+            environment.register(name, type, node)
+            node
+        }
+        is ASTDecl.Struct.Record -> TODO()
+        is ASTDecl.Struct.Tuple -> TODO()
+    }
+}
+
+fun ASTExpr.analyze(environment: Environment, expectedType: Type? = null): Expr {
+    return when (this) {
+        is ASTExpr.Binary -> {
+            val analyzedLeft = left.analyze(environment)
+            val analyzedRight = right.analyze(environment)
+            val type = when (op) {
+                "+", "-", "*", "/" -> {
+                    if (analyzedLeft.type !is Type.Primitive.Number) {
+                        Type.Error.Other("Expected number, got ${analyzedLeft.type}", analyzedLeft.range)
+                    } else if (analyzedRight.type !is Type.Primitive.Number) {
+                        Type.Error.Other("Expected number, got ${analyzedRight.type}", analyzedRight.range)
+                    } else {
+                        analyzedLeft.type
+                    }
+                }
+                "==" -> {
+                    if (analyzedLeft.type != analyzedRight.type) {
+                        Type.Error.Other("Expected ${analyzedLeft.type}, got ${analyzedRight.type}", analyzedRight.range)
+                    } else {
+                        Type.Primitive.Boolean
+                    }
+                }
+                else -> Type.Error.Other("Unknown binary operator `${op}`", this.range)
+            }
+            Expr.Binary(analyzedLeft, op, analyzedRight, type, range)
+        }
+        is ASTExpr.Call.Record -> TODO()
+        is ASTExpr.Call.Tuple -> TODO()
+        is ASTExpr.Identifier -> {
+            Expr.Identifier(
+                name,
+                when (val actualType = environment.findType(name)) {
+                    null -> Type.Error.UnboundName(name, this.range)
+                    is Type.Error -> Type.Error.Propagated(actualType)
+                    else -> assertEquals(actualType, expectedType, this.range)
+                },
+                range
+            )
+        }
+
+        is ASTExpr.Literal.Float -> Expr.Literal.Float(value, range)
+        is ASTExpr.Literal.Int -> Expr.Literal.Int(value, range)
+        is ASTExpr.Literal.String -> Expr.Literal.String(value, range)
+        is ASTExpr.Match -> TODO()
+        is ASTExpr.Unary -> {
+            val analyzedOperand = operand.analyze(environment)
+            when (op) {
+                "+", "-" -> {
+                    val type = if (analyzedOperand.type !is Type.Primitive.Number) {
+                        Type.Error.Other("Expected number, got ${analyzedOperand.type}", analyzedOperand.range)
+                    } else {
+                        analyzedOperand.type
+                    }
+                    Expr.Unary(op, analyzedOperand, type, range)
+                }
+                "!" -> {
+                    val type = if (analyzedOperand.type !is Type.Primitive.Boolean) {
+                        Type.Error.Other("Expected boolean, got ${analyzedOperand.type}", analyzedOperand.range)
+                    } else {
+                        Type.Primitive.Boolean
+                    }
+                    Expr.Unary(op, analyzedOperand, type, range)
+                }
+                else -> Expr.Unary(op, analyzedOperand, Type.Error.Other("Unknown unary operator `${op}`", this.range), range)
+            }
+        }
+    }
+}
+
+fun ASTPattern.analyze(environment: Environment, expectedType: Type? = null): Pattern {
+    return when (this) {
+        is ASTPattern.Binary -> TODO()
+        is ASTPattern.Literal.Float -> TODO()
+        is ASTPattern.Literal.Int -> TODO()
+        is ASTPattern.Struct.Record -> TODO()
+        is ASTPattern.Literal.String -> TODO()
+        is ASTPattern.Struct.Tuple -> TODO()
+        is ASTPattern.Identifier -> {
+            if (name == "_") {
+                return Pattern.Wildcard(expectedType ?: Type.Error.Other("Unable to infer type of wildcard", this.range), range)
+            }
+            when (val environmentType = environment.findType(name)) {
+                is Type.StructConstructor -> Pattern.Struct.Bare(name, environmentType, range)
+                else -> {
+                    val type = expectedType ?: Type.Error.Other("Unable to infer type of `$name`", this.range)
+                    Pattern.NameBinding(name, type, range).also {
+                        environment.register(name, type, it)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun assertEquals(actual: Type, expected: Type?, range: Range): Type {
+    if (expected is Type.Error) {
+        return Type.Error.Propagated(expected)
+    }
+    if (expected == null) {
+        return actual
+    }
+    if (actual == expected) {
+        return actual
+    }
+    return Type.Error.Other("Expected $expected, got $actual", range)
+}
---
+++ src/commonMain/kotlin/cael/analysis/SG.kt
@@ -0,0 +1,453 @@
+package cael.analysis
+
+import cael.ast.Range
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+sealed interface Type {
+    @Serializable
+    data object Any
+
+    @Serializable
+    sealed interface Primitive : Type {
+        @Serializable
+        sealed interface Number : Primitive
+
+        @Serializable
+        data object Int : Number
+
+        @Serializable
+        data object Float : Number
+
+        @Serializable
+        data object String : Primitive
+
+        @Serializable
+        data object Boolean : Primitive
+    }
+
+    sealed interface StructConstructor : Type {
+        @Serializable
+        data class Bare(
+            val name: String,
+        ) : StructConstructor
+
+        @Serializable
+        data class Tuple(
+            val name: String,
+            val components: List<Type>,
+        ) : StructConstructor
+
+        @Serializable
+        data class Record(
+            val name: String,
+            val components: List<TypeRecordItem>,
+        ) : StructConstructor
+    }
+
+    @Serializable
+    data class Struct(val node: Decl.Struct) : Type
+
+    @Serializable
+    sealed interface Fun : Type {
+        @Serializable
+        data class Tuple(
+            val node: Decl.Fun.Tuple
+        ) : Fun
+
+        @Serializable
+        data class Record(
+            val node: Decl.Fun.Record
+        ) : Fun
+
+        @Serializable
+        data class Match(
+            val node: Decl.Fun.Match
+        ) : Fun
+    }
+
+    sealed interface Error : Type, StructConstructor {
+        fun primaryError(): Primary
+
+        sealed interface Primary : Error {
+            fun message(fileName: String, fileContents: String): String
+        }
+
+        data class UnboundName(
+            val name: String,
+            val range: Range,
+        ) : Primary {
+            override fun primaryError(): Primary = this
+
+            override fun message(fileName: String, fileContents: String): String {
+                return """
+                    Unbound name `$name` at ${range.start}:${range.start}
+                """.trimIndent()
+            }
+        }
+
+        data class Other(
+            val message: String,
+            val range: Range,
+        ) : Primary {
+            override fun primaryError(): Primary = this
+
+            override fun message(fileName: String, fileContents: String): String {
+                TODO("Not yet implemented")
+            }
+        }
+
+        data class Propagated(
+            val parent: Error
+        ) : Error {
+            override fun primaryError(): Primary {
+                var next = this.parent
+                while (next !is Primary) {
+                    if (next !is Propagated) {
+                        throw Error("Unkown error type")
+                    } else {
+                        next = next.parent
+                    }
+                }
+                return next
+            }
+        }
+    }
+
+    data class Union(
+        val types: List<Type>,
+    ) : Type
+}
+
+@Serializable
+sealed interface Node {
+    val range: Range
+}
+
+@Serializable
+data class Program(
+    val declarations: List<Decl>,
+    override val range: Range,
+) : Node
+
+@Serializable
+sealed interface Decl : Node {
+
+    @Serializable
+    sealed interface Struct : Decl {
+        val constructorType: Type.StructConstructor
+
+        @Serializable
+        data class Bare(
+            override val constructorType: Type.StructConstructor.Bare,
+            override val range: Range,
+        ) : Struct
+
+        @Serializable
+        data class Tuple(
+            override val constructorType: Type.StructConstructor.Tuple,
+            override val range: Range,
+        ) : Struct
+
+        @Serializable
+        data class Record(
+            override val constructorType: Type.StructConstructor.Tuple,
+            override val range: Range,
+        ) : Struct
+    }
+
+    @Serializable
+    data class Let(
+        val pattern: Pattern,
+        val value: Expr,
+        override val range: Range,
+    ) : Decl
+
+    @Serializable
+    sealed interface Fun : Decl {
+        @Serializable
+        data class Tuple(
+            val name: String,
+            val parameters: List<Pattern>,
+            val value: Expr,
+            override val range: Range,
+        ) : Fun
+
+        @Serializable
+        data class Record(
+            val name: String,
+            val parameters: List<PatternRecordItem>,
+            val value: Expr,
+            override val range: Range,
+        ) : Fun
+
+        @Serializable
+        data class Match(
+            val name: String,
+            val cases: List<Case>,
+            override val range: Range,
+        ) : Fun {
+            sealed interface Case : Node {
+                @Serializable
+                data class Tuple(
+                    val parameters: List<Pattern>,
+                    val value: Expr,
+                    override val range: Range,
+                ) : Case
+
+                @Serializable
+                data class Record(
+                    val parameters: List<PatternRecordItem>,
+                    val value: Expr,
+                    override val range: Range,
+                ) : Case
+            }
+        }
+    }
+}
+
+@Serializable
+sealed interface Expr : Node {
+    val type: Type
+
+    @Serializable
+    data class Identifier(
+        val name: String,
+        override val type: Type,
+        override val range: Range,
+    ) : Expr
+
+    @Serializable
+    sealed interface Literal : Expr {
+        @Serializable
+        data class Int(
+            val value: kotlin.Int,
+            override val range: Range,
+        ) : Literal {
+            override val type = Type.Primitive.Int
+        }
+
+        @Serializable
+        data class Float(
+            val value: Double,
+            override val range: Range,
+        ) : Literal {
+            override val type = Type.Primitive.Float
+        }
+
+        @Serializable
+        data class String(
+            val value: kotlin.String,
+            override val range: Range,
+        ) : Literal {
+            override val type = Type.Primitive.String
+        }
+    }
+
+    @Serializable
+    sealed interface Call : Expr {
+        @Serializable
+        data class Tuple(
+            val callee: Expr,
+            val arguments: List<Expr>,
+            override val type: Type,
+            override val range: Range,
+        ) : Call
+
+        @Serializable
+        data class Record(
+            val callee: Expr,
+            val arguments: List<ExprRecordItem>,
+            override val type: Type,
+            override val range: Range,
+        ) : Call
+    }
+
+    @Serializable
+    data class Binary(
+        val left: Expr,
+        val op: String,
+        val right: Expr,
+        override val type: Type,
+        override val range: Range,
+    ) : Expr
+
+    @Serializable
+    data class Unary(
+        val op: String,
+        val operand: Expr,
+        override val type: Type,
+        override val range: Range,
+    ) : Expr
+
+    @Serializable
+    data class Match(
+        val expr: Expr,
+        val cases: List<Case>,
+        override val type: Type,
+        override val range: Range,
+    ) : Expr {
+        @Serializable
+        data class Case(
+            val pattern: Pattern,
+            val value: Expr,
+            val type: Type,
+            override val range: Range,
+        ) : Node
+    }
+}
+
+@Serializable
+sealed interface Pattern : Node {
+    val typeMatched: Type
+    fun boundNames(): List<BoundName>
+    
+    @Serializable
+    data class BoundName(
+        val name: String,
+        val type: Type,
+        val node: Node,
+    )
+
+    @Serializable
+    data class NameBinding(
+        val name: String,
+        override val typeMatched: Type,
+        override val range: Range,
+    ) : Pattern {
+        override fun boundNames(): List<BoundName> {
+            return listOf(BoundName(name, typeMatched, this))
+        }
+    }
+
+    @Serializable
+    data class Wildcard(
+        override val typeMatched: Type,
+        override val range: Range,
+    ) : Pattern {
+        override fun boundNames(): List<BoundName> {
+            return emptyList()
+        }
+    }
+
+    @Serializable
+    sealed interface Literal {
+        data class Int(
+            val value: kotlin.Int,
+            override val range: Range,
+        ) : Pattern {
+            override val typeMatched = Type.Primitive.Int
+
+            override fun boundNames(): List<BoundName> {
+                return emptyList()
+            }
+        }
+
+        @Serializable
+        data class Float(
+            val value: Double,
+            override val range: Range,
+        ) : Pattern {
+            override val typeMatched = Type.Primitive.Float
+
+            override fun boundNames(): List<BoundName> {
+                return emptyList()
+            }
+        }
+
+        @Serializable
+        data class String(
+            val value: kotlin.String,
+            override val range: Range,
+        ) : Pattern {
+            override val typeMatched = Type.Primitive.String
+
+            override fun boundNames(): List<BoundName> {
+                return emptyList()
+            }
+        }
+    }
+
+    @Serializable
+    sealed interface Struct {
+        @Serializable
+        data class Bare(
+            val name: String,
+            override val typeMatched: Type.StructConstructor,
+            override val range: Range,
+        ) : Pattern {
+            override fun boundNames(): List<BoundName> {
+                return emptyList()
+            }
+        }
+
+        @Serializable
+        data class Tuple(
+            val name: String,
+            val components: List<Pattern>,
+            override val typeMatched: Type.StructConstructor,
+            override val range: Range,
+        ) : Pattern {
+            override fun boundNames(): List<BoundName> {
+                return components.flatMap { it.boundNames() }
+            }
+        }
+
+        @Serializable
+        data class Record(
+            val name: String,
+            val components: List<PatternRecordItem>,
+            override val typeMatched: Type.StructConstructor,
+            override val range: Range,
+        ) : Pattern {
+            override fun boundNames(): List<BoundName> {
+                return components.flatMap { it.pattern.boundNames() }
+            }
+        }
+    }
+
+    data class Binary(
+        val left: Pattern,
+        val op: String,
+        val right: Pattern,
+        override val typeMatched: Type,
+        override val range: Range,
+    ) : Pattern {
+        override fun boundNames(): List<BoundName> {
+            return left.boundNames() + right.boundNames()
+        }
+    }
+
+    data class Unary(
+        val op: String,
+        val operand: Pattern,
+        override val typeMatched: Type,
+        override val range: Range,
+    ) : Pattern {
+        override fun boundNames(): List<BoundName> {
+            return operand.boundNames()
+        }
+    }
+}
+
+@Serializable
+data class TypeRecordItem(
+    val name: String,
+    val type: Type,
+    override val range: Range,
+) : Node
+
+@Serializable
+data class ExprRecordItem(
+    val name: String,
+    val value: Expr,
+    override val range: Range,
+) : Node
+
+@Serializable
+data class PatternRecordItem(
+    val name: String,
+    val pattern: Pattern,
+    override val range: Range,
+) : Node
