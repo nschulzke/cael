07f262ae48836a64a39efe8444f35d062668316f
Nathan Schulzke <nschulzke@gmail.com>
2023-11-15 22:04:42 -0700

    Analyze tuple struct invocations

--- src/commonTest/kotlin/cael/analysis/AnalysisTests.kt
+++ src/commonTest/kotlin/cael/analysis/AnalysisTests.kt
@@ -17,12 +17,12 @@ class AnalysisTests : DescribeSpec({
             program.declarations[0].apply {
                 this.shouldBeTypeOf<Decl.Let>()
                 range shouldBeEqual Range(1, 9)
-                pattern.typeMatched shouldBeEqual Type.Primitive.Int
+                pattern.typeMatched shouldBeEqual Type.Primitive.Int.struct
                 pattern.boundNames().apply {
                     size shouldBeEqual 1
                     this[0].apply {
                         name shouldBeEqual "x"
-                        type shouldBeEqual Type.Primitive.Int
+                        type shouldBeEqual Type.Primitive.Int.struct
                     }
                 }
                 value shouldBeEqual Expr.Literal.Int(5, Range(9, 1))
@@ -37,12 +37,12 @@ class AnalysisTests : DescribeSpec({
             program.declarations[0].apply {
                 this.shouldBeTypeOf<Decl.Let>()
                 range shouldBeEqual Range(1, 21)
-                pattern.typeMatched shouldBeEqual Type.Primitive.String
+                pattern.typeMatched shouldBeEqual Type.Primitive.String.struct
                 pattern.boundNames().apply {
                     size shouldBeEqual 1
                     this[0].apply {
                         name shouldBeEqual "x"
-                        type shouldBeEqual Type.Primitive.String
+                        type shouldBeEqual Type.Primitive.String.struct
                     }
                 }
                 value shouldBeEqual Expr.Literal.String("hello world", Range(9, 13))
@@ -76,6 +76,51 @@ class AnalysisTests : DescribeSpec({
                 }
                 value shouldBeEqual Expr.Identifier("Foo", expectedType, Range(20, 3))
             }
+        }
+
+        it("binds a name to a full tuple struct") {
+            val environment = rootEnvironment()
+            val program = """
+                struct Foo(Int)
+                let x = Foo(1)
+            """.trimIndent().parse().analyze(environment)
+            program.declarations.size shouldBeEqual 2
+            val expectedConstructorType = Type.StructConstructor.Tuple("Foo", listOf(Type.Primitive.Int.struct))
+            val expectedType = expectedConstructorType.struct
+
+            program.declarations[0].apply {
+                this.shouldBeTypeOf<Decl.Struct.Tuple>()
+                constructorType shouldBeEqual expectedConstructorType
+            }
+            program.declarations[1].apply {
+                this.shouldBeTypeOf<Decl.Let>()
+                pattern.typeMatched shouldBeEqual expectedType
+                pattern.boundNames().apply {
+                    size shouldBeEqual 1
+                    this[0].apply {
+                        name shouldBeEqual "x"
+                        type shouldBeEqual expectedType
+                    }
+                }
+                value.apply {
+                    shouldBeTypeOf<Expr.Call.Tuple>()
+                    callee.apply {
+                        shouldBeTypeOf<Expr.Identifier>()
+                        name shouldBeEqual "Foo"
+                        type shouldBeEqual expectedConstructorType
+                    }
+                    arguments.apply {
+                        size shouldBeEqual 1
+                        this[0].apply {
+                            shouldBeTypeOf<Expr.Literal.Int>()
+                            value shouldBeEqual 1
+                            type shouldBeEqual Type.Primitive.Int.struct
+                        }
+                    }
+                    type shouldBeEqual expectedType
+                }
+            }
+            environment.findType("x")!! shouldBeEqual expectedType
         }
     }
 })
